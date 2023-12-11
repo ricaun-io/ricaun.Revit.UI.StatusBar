ae0ba91e93fb61167f05da6afcffbcbee488b381
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-12-11 12:27:17 -0300

    Update prerelease

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack
+class Build : NukeBuild, IPublishPack, IPrePack
 {
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
